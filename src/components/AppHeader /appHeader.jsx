2ca617b78cec8037859be44359107915ff4809d0
iarosch22 <67790339+iarosch22@users.noreply.github.com>
2023-07-19 13:00:33 +0000

    feat: add appheader

---
+++ src/components/AppHeader /appHeader.jsx
@@ -0,0 +1,10 @@
+import styles from './appheader.module.css';
+
+function AppHeader() {
+    return (
+      <header className={`${styles.header} p-1`}>
+      </header>
+    );
+}
+
+export default AppHeader;
