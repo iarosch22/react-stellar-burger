8f8526a2280a24ef3e06530e935b51fb5d32c378
iarosch22 <67790339+iarosch22@users.noreply.github.com>
2023-09-04 13:53:35 +0000

    refactor: appheader

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -1,7 +1,7 @@
 import styles from './appheader.module.css';
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
-import Nav from '../nav/nav';
-import UserBox from '../userBox/userBox';
+import Nav from './nav/nav';
+import UserBox from './userBox/userBox';
 
 function AppHeader() {
     return (
